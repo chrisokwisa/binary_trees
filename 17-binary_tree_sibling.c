4bfa95dee9434d412dcf79ecba5b4cecdc787bdb
chrisokwisa <okwisachristabel@gmail.com>
2022-08-25 10:47:43 +0000

    Finds the sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,14 +9,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *sibling;
-
-	if (node == NULL || node->parent == NULL
-	|| node->parent->right || node->parent->left)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	sibling = node->parent->right;
-	sibling = (node == sibling) ? node->parent->left : sibling;
+	if (node->parent->left == node)
+		return (node->parent->right);
 
-	return (sibling);
+	return (node->parent->left);
 }
