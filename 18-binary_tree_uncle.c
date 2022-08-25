f81bec4b774768a508ce44c1ce24f77d27d4c463
chrisokwisa <okwisachristabel@gmail.com>
2022-08-25 11:07:51 +0000

    Finds the uncle in binary tree

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,22 +2,20 @@
 
 /**
  * binary_tree_uncle - Finds th uncle of a node
- * @node: pointer to the node find the uncle
+ * @node: pointer to the node to find the uncle
  * Desc: Function must return a pointer to the uncle node
  * Return: If node is NULL, return NULL
  *	ifnode has no uncle, return NULL
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *uncle;
+	if (node == NULL ||
+	node->parent == NULL ||
+	node->parent->parent == NULL)
+		return (NULL);
 
-	if (node == NULL || node->parent->parent
-	|| node->parent->parent->left
-	|| node->parent->parent->right)
-		return (0);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
 
-	uncle = node->parent->parent->right;
-	uncle = (node->parent == uncle) ? node->parent->parent->left : uncle;
-
-	return (uncle);
+	return (node->parent->parent->left);
 }
